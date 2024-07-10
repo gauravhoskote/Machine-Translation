0afb92b66b6bda06c05976e0bc92d2ca098fcbd0
Gaurav Hoskote <46561972+gauravhoskote@users.noreply.github.com>
2024-07-09 18:08:44 -0700

    Update seq2seq_transformer.py

--- seq2seq_transformer.py
+++ seq2seq_transformer.py
@@ -1,15 +1,3 @@
-"""
-Seq2Seq using Transformers on the Multi30k
-dataset. In this video I utilize Pytorch
-inbuilt Transformer modules, and have a
-separate implementation for Transformers
-from scratch. Training this model for a
-while (not too long) gives a BLEU score
-of ~35, and I think training for longer
-would give even better results.
-
-"""
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
